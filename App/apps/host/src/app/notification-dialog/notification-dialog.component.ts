5a96059301636bf84598152e0d9daf9614f6da93
Karatist <skammoshenik@gmail.com>
2024-07-05 23:36:59 +0300

    Create base componetn, without kendo ui(so hard)

--- App/apps/host/src/app/notification-dialog/notification-dialog.component.ts
+++ App/apps/host/src/app/notification-dialog/notification-dialog.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-//import { DialogService } from '@progress/kendo-angular-dialog';
 
 @Component({
   selector: 'app-notification-dialog',
   templateUrl: './notification-dialog.component.html',
   styleUrls: ['./notification-dialog.component.scss']
 })
-export class NotificationDialogComponent  {
+export class NotificationDialogComponent {
   public people = [
     { id: 1, name: 'John Doe' },
     { id: 2, name: 'Jane Smith' },
@@ -16,6 +15,4 @@ export class NotificationDialogComponent  {
     { id: 1, from: 'John Doe', text: 'Hello!', timestamp: new Date() },
     { id: 2, from: 'Jane Smith', text: 'Hi there!', timestamp: new Date() },
   ];
-
-
 }
