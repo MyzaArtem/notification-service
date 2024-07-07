5a413f9be04e5f2f2da3b6b841577cb048338b0d
Karatist <skammoshenik@gmail.com>
2024-07-07 11:18:01 +0300

    add fronted

---
+++ App/apps/host/src/app/notification-dialog/notification-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-dialog',
+  templateUrl: './notification-dialog.component.html',
+  styleUrls: ['./notification-dialog.component.scss']
+})
+export class NotificationDialogComponent {
+  public people = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+  ];
+
+  public messages = [
+    { id: 1, from: 'John Doe', text: 'Hello!', timestamp: new Date() },
+    { id: 2, from: 'Jane Smith', text: 'Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there!  Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! Hi there! ', timestamp: new Date() },
+  ];
+}
